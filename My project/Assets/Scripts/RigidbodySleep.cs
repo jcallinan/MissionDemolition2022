7653b9d6112ba30c6fb2b5988b22424bee0bf32e
Jeremy Callinan <jeremy.callinan@gmail.com>
2022-10-11 20:08:22 -0400

    10112022 Changes

---
+++ My project/Assets/Scripts/RigidbodySleep.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
+public class RigidbodySleep : MonoBehaviour
+{
+    private int sleepCountDown = 4;
+    private Rigidbody rigid;
+
+
+
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+    private void FixedUpdate()
+    {
+        if (sleepCountDown > 0)
+        {
+            rigid.Sleep();
+            sleepCountDown--;
+        }
+    }
+
+}
